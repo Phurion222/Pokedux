609ac56aecfdca2367119c908f399e9065577cfd
Phurion222 <68572619+Phurion222@users.noreply.github.com>
2024-09-20 08:59:14 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { pokeonsReducer } from './reducers/pokemons';
+import { Provider } from'react-redux';
+import { legacy_createStore as createStore } from'redux';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(pokeonsReducer);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
